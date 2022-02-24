b93716dfe8bda4074ca2b1085c0f5a4a74bf4c71
CodeLogic <53108370+hdgaadd@users.noreply.github.com>
2022-02-24 23:57:13 +0800

    finished constants

--- Mybatis/src/main/java/com/codeman/constants/Constant.java
+++ Mybatis/src/main/java/com/codeman/constants/Constant.java
@@ -10,6 +10,9 @@ public interface Constant {
      */
     String CHARSET = "UTF-8";
 
+    /**
+     * properties文件
+     */
     String MAPPER_LOCATION = "mapper.location";
 
     String DB_DRIVER_CONF = "db.driver";
@@ -17,6 +20,19 @@ public interface Constant {
     String DB_URL_CONF = "db.url";
 
     String DB_PASSWORD = "db.password";
+
+    /**
+     * mapper文件后缀
+     */
+    String MAPPER_FILE_SUFFIX = ".xml"; // [ˈsʌfɪks]后缀
+
+    String XML_ROOT_LABEL = "mapper"; // [ˈleɪb(ə)l]标签
+
+    String XML_ELEMENT_ID = "id";
+
+    String XML_SELECT_NAMESPACE = "namespace";
+
+    String XML_SELECT_RESULTTYPE = "resultType";
 
 
 }
--- Mybatis/src/main/java/com/codeman/constants/SqlType.java
+++ Mybatis/src/main/java/com/codeman/constants/SqlType.java
@@ -3,7 +3,22 @@ package com.codeman.constants;
 /**
  * @author hdgaadd
  * Created on 2022/02/23
+ *
+ * SQL操作类型
  */
 public enum SqlType {
+    SELECT("select"),
+    INSERT("insert"),
+    UPDATE("update"),
+    DEFAULT("default");
 
+    private String value;
+
+    SqlType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
